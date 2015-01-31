9e60b09ba14db2fd5b682f03483599589b29f9fc
inkybeta <inkybeta@outlook.com>
2015-01-30 19:21:26 -0800

    Added a few more projects to be able to seperate concerns. This project will follow the "onion" pattern. In addition, necesary dependencies were added.

---
+++ InkyBeta.Data/DataContext/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InkyBeta.Core;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace InkyBeta.Data.DataContext
+{
+    public class UserContext : IdentityDbContext<User>
+    {
+	    public UserContext() : base("UserConnected")
+	    {
+		    
+	    }
+    }
+}
